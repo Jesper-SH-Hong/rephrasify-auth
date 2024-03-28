7b30231f278e89026065f4dfb5b685ea62f09add
Jesper (Sung Hyun) Hong <78632105+Jesper-SH-Hong@users.noreply.github.com>
2024-03-27 17:35:05 -0700

    wire up startup file

--- api-gateway/app.js
+++ api-gateway/app.js
@@ -1,12 +1,5 @@
 const http = require("http");
 const url = require("url");
-
-// const STATUS = {
-//   OK: 200,
-//   BAD_REQUEST: 400,
-//   NOT_FOUND: 404,
-// };
-
 const mysql = require("mysql");
 const db_admin = mysql.createConnection({
   host: "localhost",
@@ -34,51 +27,11 @@ class Server {
       res.setHeader("Access-Control-Allow-Methods", "POST, OPTIONS");
 
       res.writeHead(200, { "Content-Type": "text/plain" });
-      let message = "It works! JESPER CUSTOM MESSAGE\n";
+      let message = "THIS IS API-GATEWAY APP\n";
       let version = "NodeJS " + process.versions.node + "\n";
       let response = [message, version].join("\n");
       res.end(response);
-
-        // if (q.pathname == `${lab5Path}/post`) {
-        //   console.log("Recieved POST request");
-        //   // handle preflight request for POST
-        //   if (req.headers["access-control-request-method"]) {
-        //     res.setHeader("Access-Control-Allow-Methods", "POST");
-        //     res.end();
-        //   } else {
-        //     let body = "";
-        //     req.on("data", (chunk) => {
-        //       body += chunk;
-        //     });
-        //     req.on("end", () => {
-        //       console.log(`server recieved post request body: ${body}`);
-        //       this.db_admin.query(body, (err, result) => {
-        //         if (err) {
-        //           const errorMessage = err.message || "Internal Server Error";
-        //           const errorResponse = {
-        //             status: "error",
-        //             message: errorMessage,
-        //           };
-        //           res.writeHead(500, { "Content-Type": "application/json" });
-        //           res.write(JSON.stringify(errorResponse));
-        //           res.end();
-        //         } else {
-        //           res.writeHead(STATUS.OK);
-        //           res.end(JSON.stringify({ status: "success", data: result }));
-        //         }
-        //       });
-        //     });
-        //   }
-        // }
     });
-
-    // close db_admin connection when server is closed
-    // server.on("close", () => {
-    //   this.db_admin.end((err) => {
-    //     if (err) throw err;
-    //     console.log("DB Connection closed.");
-    //   });
-    // });
 
     server.listen(this.port);
   }
