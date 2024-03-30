9bf5781676feac6c8bfd0bfe1c6fd40f029e8b22
Kimmm-c <97310663+Kimmm-c@users.noreply.github.com>
2024-03-30 16:27:11 -0700

    swapped local db to remote

--- app.js
+++ app.js
@@ -33,14 +33,10 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 const db_admin = mysql.createConnection({
-  // host: process.env.DB_HOST,
-  // user: process.env.DB_USER,
-  // password: process.env.DB_PASSWORD,
-  // database: process.env.DB_NAME
-  host: process.env.DB_LOCAL_HOST,
-  user: process.env.DB_LOCAL_USER,
-  password: process.env.DB_LOCAL_PASSWORD,
-  database: process.env.DB_LOCAL_NAME
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME
 });
 
 class User {
