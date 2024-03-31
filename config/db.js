773154640bc847b963b2c53d5ea410df68612308
Kimmm-c <97310663+Kimmm-c@users.noreply.github.com>
2024-03-30 22:25:40 -0700

    swapped db

--- config/db.js
+++ config/db.js
@@ -1,13 +1,9 @@
 const mysql = require('mysql');
 require("dotenv").config();
 const db_admin = mysql.createConnection({
-    // host: process.env.DB_HOST,
-    // user: process.env.DB_USER,
-    // password: process.env.DB_PASSWORD,
-    // database: process.env.DB_NAME
-    host: process.env.DB_LOCAL_HOST,
-    user: process.env.DB_LOCAL_USER,
-    password: process.env.DB_LOCAL_PASSWORD,
-    database: process.env.DB_LOCAL_NAME
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
 });
 module.exports = db_admin;
